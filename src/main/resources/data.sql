890799e6b6d595e5b9df415aac82dd74a883077c
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-08-11 19:35:53 +0900

    Add Sample Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, first_name, last_name, email) VALUES (1, 'bar1', 'foo1', 'foobar1@yahoo.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES (2, 'bar2', 'foo2', 'foobar2@yahoo.com');
+INSERT INTO users (id, first_name, last_name, email) VALUES (3, 'bar3', 'foo3', 'foobar3@yahoo.com');
